4761ff2146a31f9cc4a5908958273a7b1f55fe7c
Chase Stubblefield <chasestubblefield1@gmail.com>
2018-10-23 15:51:56 -0700

    Mostly done. Got highlighting ship select working. Commiting to save changes

--- src/main/java/assets/game.js
+++ src/main/java/assets/game.js
@@ -5,17 +5,6 @@ var shipType;
 var vertical;
 var gg = false;  // Is the game over or not
 
-function highlightSelectedShip(){
-    var ships = document.querySelector("ships");
-    var btns = ships.getElementsByClassName("btn");
-    for (var i = 0; i < btns.length; i++) {
-      btns[i].addEventListener("click", function() {
-        var current = document.querySelector("btnactive");
-        current[0].classList.remove("btnactive");
-        this.classList.add("btnactive");
-      });
-    }
-}
 
 function makeGrid(table, isPlayer) {
     for (i=0; i<10; i++) {
@@ -63,8 +52,10 @@ function redrawGrid() {
         document.getElementById("player").rows[square.row-1].cells[square.column.charCodeAt(0) - 'A'.charCodeAt(0)].classList.add("occupied");
         document.getElementById("player_copy").rows[square.row-1].cells[square.column.charCodeAt(0) - 'A'.charCodeAt(0)].classList.add("occupied");
     }));
-    markHits(game.opponentsBoard, "opponent", "You won the game");
-    markHits(game.playersBoard, "player", "You lost the game");
+
+    /* text displayed for user in error modal upon winning/losing */
+    markHits(game.opponentsBoard, "opponent", "Congratulations! You Won!");
+    markHits(game.playersBoard, "player", "You Lost.");
 }
 
 var oldListener;
@@ -120,39 +111,45 @@ function sendXhr(method, url, data, handler) {
     req.setRequestHeader("Content-Type", "application/json");
     req.send(JSON.stringify(data));
 }
+
 // this function is code removed from place function so it could be reused other than that Reese didn't touch it
 function finishPlacement(size,table, vertical){
- for (let i=0; i<size; i++) {// goes through for the length of the ship being placed
-            let cell;// name for the current space on board that the board is trying to hilight
-            if(vertical) {//if the ship will be placed vertically increment through the rows
+    for (let i=0; i<size; i++) { // goes through for the length of the ship being placed
+            let cell; // name for the current space on board that the board is trying to hilight
+            if(vertical) { //if the ship will be placed vertically increment through the rows
                 let tableRow = table.rows[row+i];
                 if (tableRow === undefined) {
                     // ship is over the edge; let the back end deal with it
                     break;
                 }
                 cell = tableRow.cells[col];
-            } else {//if the ship isn't vertical increment through the columns
+            } else { //if the ship isn't vertical increment through the columns
                 cell = table.rows[row].cells[col+i];
             }
             if (cell === undefined) {
                 // ship is over the edge; let the back end deal with it
                 break;
             }
-            cell.classList.toggle("placed");//add class placed to cell so it will show up with color on the board!
-        }
-  }
+            cell.classList.toggle("placed"); // add class placed to cell so it will show up with color on the board!
+    }
+}
+
 //this is not a function I wrote but comments were asked for it so i will do my best
 function place(size) {
     return function() {
-        let row = this.parentNode.rowIndex;// i believe these lines get the info from the cell clicked so ships will be placed in the right location
+        let row = this.parentNode.rowIndex; // I believe these lines get the info from the cell clicked so ships will be placed in the right location
         let col = this.cellIndex;
-        vertical = document.getElementById("is_vertical").checked;// this recieves info on weather or not the play check the box for vertical
-                                                                  //******** it will need to be chnged for the arrow keys to work****************************************************
-        let table = document.getElementById("player");// this makes sure the right table is selected for ship placement
-        let table2 = document.getElementById("player_copy");// i added this so player copy will be able to have the ships placed just like player
-        finishPlacement(size,table, vertical);//finish placement is not a function I wrote, the lines of code it contains used to be a part of place function
-                                    //but I needed it to place for both player and player copy so instead of just copying all the code I took what
-                                    //was already there and put it into a funtion so I could reuse the code.
+
+        // this receives info on weather or not the play check the box for vertical
+        // *** it will need to be changed for the arrow keys to work ****
+        vertical = document.getElementById("is_vertical").checked;
+        let table = document.getElementById("player"); // this makes sure the right table is selected for ship placement
+        let table2 = document.getElementById("player_copy"); // i added this so player copy will be able to have the ships placed just like player
+
+        // finish placement is not a function I wrote, the lines of code it contains used to be a part of place function
+        // but I needed it to place for both player and player copy so instead of just copying all the code I took what
+        // was already there and put it into a function so I could reuse the code.
+        finishPlacement(size,table, vertical);
         finishPlacement(size,table2, vertical);
 
     }
@@ -162,40 +159,50 @@ function initGame() {
     makeGrid(document.getElementById("player_copy"), true);
     makeGrid(document.getElementById("opponent"), false);
     makeGrid(document.getElementById("player"), true);
+
+    shipType = "BATTLESHIP";
+
     document.getElementById("place_minesweeper").addEventListener("click", function(e) {
-        shipType = "MINESWEEPER";
+       shipType = "MINESWEEPER";
        registerCellListener(place(2));
+       document.getElementsByClassName("btnactive")[0].classList.remove("btnactive");
+       this.classList.add("btnactive");
     });
+
     document.getElementById("place_destroyer").addEventListener("click", function(e) {
-        shipType = "DESTROYER";
+       shipType = "DESTROYER";
        registerCellListener(place(3));
+       document.getElementsByClassName("btnactive")[0].classList.remove("btnactive");
+       this.classList.add("btnactive");
     });
+
     document.getElementById("place_battleship").addEventListener("click", function(e) {
-        shipType = "BATTLESHIP";
+       shipType = "BATTLESHIP";
        registerCellListener(place(4));
+       document.getElementsByClassName("btnactive")[0].classList.remove("btnactive");
+       this.classList.add("btnactive");
     });
+
     sendXhr("GET", "/game", {}, function(data) {
         game = data;
     });
 };
 
-//Opens the invalid move modal.
+// open invalid move modal
 function openInv(){
 	document.getElementById("modal-backdrop-inv").classList.remove("inactive");
 	document.getElementById("modal-inv").classList.remove("inactive");
 }
 
-//closes the invalid move modal.
+// closes invalid move modal
 function closeInv(){
 	document.getElementById("modal-backdrop-inv").classList.add("inactive");
 	document.getElementById("modal-inv").classList.add("inactive");
 }
 
-//Closes the invalid move modal when one of the buttons is clicked
+// close invalid move modal when one of the buttons is clicked
 document.getElementsByClassName("modal-close-button-inv")[0].addEventListener("click",closeInv);
 document.getElementsByClassName("modal-okay-button-inv")[0].addEventListener("click",closeInv);
-
-
 
 //Opens the game over modal.
 function openGG(surrenderText){
@@ -204,12 +211,16 @@ function openGG(surrenderText){
 	document.getElementById("surrenderText").textContent = surrenderText;
 }
 
-//closes the game over modal.
+/* currently commented out since we want the game over modal to terminate the game.
+   leaving code in for potential future use */
+
+/*//closes game over modal
 function closeGG(){
 	document.getElementById("modal-backdrop-gg").classList.add("inactive");
 	document.getElementById("modal-gg").classList.add("inactive");
-}
+}*/
 
-//Closes the game over modal when one of the buttons is clicked
+/*
+// closes game over modal when one of the buttons is clicked
 document.getElementsByClassName("modal-close-button-gg")[0].addEventListener("click",closeGG);
-document.getElementsByClassName("modal-okay-button-gg")[0].addEventListener("click",closeGG);
+document.getElementsByClassName("modal-okay-button-gg")[0].addEventListener("click",closeGG);*/
